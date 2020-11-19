d76b66af99967be40a00b04fc3a1d196db7d2f31
ikcilrep <frajerzycki@protonmail.com>
2020-11-19 22:48:59 +0100

    Create authorized page

---
+++ src/Components/MainPage.js
@@ -0,0 +1,12 @@
+import Navbar from "./Authorized/Navbar";
+import React from "react";
+
+const MainPage = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default MainPage;
