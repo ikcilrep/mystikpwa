d9520eb11f3022b71971b4ed36eaaab244f771bb
ikcilrep <frajerzycki@protonmail.com>
2020-11-19 22:49:06 +0100

    Create unauthorized page

---
+++ src/Components/LoginPage.js
@@ -0,0 +1,12 @@
+import Navbar from "./Unauthorized/Navbar";
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default LoginPage;
