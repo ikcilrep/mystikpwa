8019e32f2cef451405d967b494eff74810739b4c
ikcilrep <frajerzycki@protonmail.com>
2020-11-19 22:23:09 +0100

    Add isAuthorized state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from "react";
-import AuthorizedNavbar from './Components/AuthorizedNavbar';
+import React, { useState } from "react";
+import AuthorizedNavbar from "./Components/AuthorizedNavbar";
+import { useCookies } from "react-cookie";
 
 function App() {
+  const [cookies, setCookie] = useCookies(["user"]);
+  const [isAuthorized, setAuthorized] = useState(
+    cookies["user"] !== undefined && cookies["user"].expirationDate < Date.now()
+  );
+
   return (
     <div>
       <AuthorizedNavbar />
