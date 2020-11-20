227ed8f9a983f3cf6cdc6d877a3d41f88c255c0c
ikcilrep <frajerzycki@protonmail.com>
2020-11-20 18:44:13 +0100

    Set authorized after authorizing

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import AuthorizePage from "./Components/Unauthorized/AuthorizePage";
 import { useCookies } from "react-cookie";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-const isTokenUpToDate = (user) => user.expirationDate < Date.now();
+const isTokenUpToDate = (user) => user.expirationDate > Date.now();
 
 function App() {
   const [cookies, setCookie] = useCookies(["user"]);
-  const [isAuthorized /* , setAuthorized */] = useState(
+  const [isAuthorized, setAuthorized] = useState(
     cookies["user"] !== undefined && isTokenUpToDate(cookies["user"])
   );
 
   const [, setUser] = useState(undefined);
 
   const handleAuthorization = ({ user, doRememberMe }) => {
+    setAuthorized(true);
     setUser(user);
     if (doRememberMe) {
       setCookie("user", user);
