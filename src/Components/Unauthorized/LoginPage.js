77833c96818341efd3fdcb88d7dafd168111409b
ikcilrep <frajerzycki@protonmail.com>
2020-11-20 18:16:24 +0100

    Make back arrow work

--- src/Components/Unauthorized/LoginPage.js
+++ src/Components/Unauthorized/LoginPage.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import LoginForm from "./LoginForm";
 import Navbar from "./Navbar";
+import { Redirect } from "react-router-dom";
 
 const LoginPage = ({ handleAuthorization }) => {
+  const [isRedirectBack, setRedirectBack] = useState(false);
+  if (isRedirectBack) {
+    return <Redirect to="/authorize" />;
+  }
+
+  const handleRedirectBack = () => setRedirectBack(true);
+
   return (
     <>
-      <Navbar isMainPage={false} />
+      <Navbar isMainPage={false} handleRedirectBack={handleRedirectBack} />
       <Grid
         container
         spacing={0}
