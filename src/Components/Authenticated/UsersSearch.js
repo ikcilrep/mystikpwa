ef27a223447b193f1e346976fa881f558600f623
ikcilrep <frajerzycki@protonmail.com>
2020-11-21 22:44:59 +0100

    Add uninviting users

--- src/Components/Authenticated/UsersSearch.js
+++ src/Components/Authenticated/UsersSearch.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from "react";
 
 import IconButton from "@material-ui/core/IconButton";
 import AddIcon from "@material-ui/icons/Add";
+import UndoIcon from "@material-ui/icons/Undo";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import axios from "axios";
 import { serverAddress } from "../../settings.json";
+import { InsertDriveFileRounded } from "@material-ui/icons";
 
 const UsersSearch = ({ user, query, connection, setUser }) => {
   const [users, setUsers] = useState([]);
@@ -36,14 +38,24 @@ const UsersSearch = ({ user, query, connection, setUser }) => {
     setUser(userCopy);
   };
 
-  const isInvited = (searchedUser) =>
-    user.invited.some((i) => i.id === searchedUser.id);
-  const isInviter = (searchedUser) =>
-    user.inviters.some((i) => i.id === searchedUser.id);
-
-  const canUserBeInvited = (user) => {
-    return !isInvited(user) && !isInviter(user);
+  const handleInvitationUndo = (foundUser) => {
+    connection.invoke("deleteInvitations", [foundUser.id]);
+    const userCopy = { ...user };
+    userCopy.invited = userCopy.invited.filter((i) => i.id !== foundUser.id);
+    setUser(userCopy);
   };
+
+  const isInvited = (foundUser) =>
+    user.invited.some((i) => i.id === foundUser.id);
+
+  const isInviter = (foundUser) =>
+    user.inviters.some((i) => i.id === foundUser.id);
+
+  const isFriend = (foundUser) =>
+    user.friends.some((i) => i.id === foundUser.id);
+
+  const canUserBeInvited = (foundUser) =>
+    !isInvited(foundUser) && !isInviter(foundUser) && !isFriend(foundUser);
 
   return (
     <div>
@@ -55,13 +67,21 @@ const UsersSearch = ({ user, query, connection, setUser }) => {
               secondary={foundUser.id}
             />
             <ListItemSecondaryAction>
-              <IconButton
-                color="secondary"
-                disabled={!canUserBeInvited(foundUser)}
-                onClick={() => handleInvitation(foundUser)}
-              >
-                <AddIcon />
-              </IconButton>
+              {canUserBeInvited(foundUser) ? (
+                <IconButton
+                  color="secondary"
+                  onClick={() => handleInvitation(foundUser)}
+                >
+                  <AddIcon />
+                </IconButton>
+              ) : (
+                <IconButton
+                  color="secondary"
+                  onClick={() => handleInvitationUndo(foundUser)}
+                >
+                  <UndoIcon />
+                </IconButton>
+              )}
             </ListItemSecondaryAction>
           </ListItem>
         ))}
